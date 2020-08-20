294072f5cb4f2518da8acd02b98fe57336e8629e
Lufri Rais Maulana <lufriraismaulana@gmail.com>
2020-08-20 18:38:03 +0700

    Delete unused library

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.home, name='blog-home'),
     path('Forecasts', views.apiForecasts, name='forecasts'),
     path('ForecastsFilter', views.apiForecastsFilter, name='forecasts-filter'),
-    path('tes', views.dump_prediksi, name='tes'),
+    # path('tes', views.dump_prediksi, name='tes'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -11,11 +11,11 @@ from decimal import Decimal
 import decimal
 
 from os.path import join as os_join
-from keras.models import load_model
-from srfapp.settings import BASE_DIR
+# from keras.models import load_model
+# from srfapp.settings import BASE_DIR
 import numpy
 import pandas
-from joblib import load
+# from joblib import load
 from sklearn.metrics import mean_squared_error
 
 class DecimalEncoder(json.JSONEncoder):
@@ -95,37 +95,37 @@ def apiForecastsFilter(request):
 
     return HttpResponse(json.dumps(res, cls=DecimalEncoder), content_type='application/json;charset=utf-8')
 
-def dump_prediksi(request):
-    scaler = load(os_join(BASE_DIR, "staticfiles/scaler.gz"))
-    md19 = load_model(os_join(BASE_DIR, "staticfiles/MD19.h5"))
-    radiasi = Radiasi.objects.filter(tanggal__range=('2016-12-16', '2019-12-31')).order_by('tanggal')
+# def dump_prediksi(request):
+#     scaler = load(os_join(BASE_DIR, "staticfiles/scaler.gz"))
+#     md19 = load_model(os_join(BASE_DIR, "staticfiles/MD19.h5"))
+#     radiasi = Radiasi.objects.filter(tanggal__range=('2016-12-16', '2019-12-31')).order_by('tanggal')
 
-    list_radiasi = []
-    list_tanggal = []
-    for i in range(len(radiasi)):
-        list_radiasi.append(radiasi[i].radiasi)
-        if(i>15):
-            list_tanggal.append(radiasi[i].tanggal)
-    # flatten data
+#     list_radiasi = []
+#     list_tanggal = []
+#     for i in range(len(radiasi)):
+#         list_radiasi.append(radiasi[i].radiasi)
+#         if(i>15):
+#             list_tanggal.append(radiasi[i].tanggal)
+#     # flatten data
     
-    data = numpy.array(list_radiasi).astype(numpy.float32)
-    data = data.reshape(1, -1)
-    data = scaler.transform(data)
+#     data = numpy.array(list_radiasi).astype(numpy.float32)
+#     data = data.reshape(1, -1)
+#     data = scaler.transform(data)
 
-    X, y = to_supervised(data[0], n_in=16, n_out=1)
+#     X, y = to_supervised(data[0], n_in=16, n_out=1)
     
-    Y = md19.predict(X)
+#     Y = md19.predict(X)
     
-    Y = scaler.inverse_transform(Y)
-    print(Y)
-    converted_value = getattr(Y, "tolist", lambda: Y)()
-    print(converted_value[0])
-    round_converted_value = [ round(value[0], 2) for value in converted_value ]
+#     Y = scaler.inverse_transform(Y)
+#     print(Y)
+#     converted_value = getattr(Y, "tolist", lambda: Y)()
+#     print(converted_value[0])
+#     round_converted_value = [ round(value[0], 2) for value in converted_value ]
 
-    for i in range(len(list_tanggal)):
-        Prediksi.objects.update_or_create(prediksitanggal=list_tanggal[i], defaults={'prediksiradiasi':round_converted_value[i]})
+#     for i in range(len(list_tanggal)):
+#         Prediksi.objects.update_or_create(prediksitanggal=list_tanggal[i], defaults={'prediksiradiasi':round_converted_value[i]})
 
-    return HttpResponse("halo")
+#     return HttpResponse("halo")
 
 def to_supervised(data, n_in, n_out):
     n_vars = 1
